6ce14912256fe59a8c68224d1779471386c78edd
balance64 <20220824@mywhitecliffe.com>
2024-09-11 11:30:59 +1200

    Add files via upload

--- python/autobattler/character.py
+++ python/autobattler/character.py
@@ -1,4 +1,5 @@
 from weapon import fists
+from health_bar import HealthBar
 
 
 class Character:
@@ -16,8 +17,9 @@ class Character:
     def attack(self, target) -> None:
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
-        print(f"{self.name} dealt {self.weapon.damage} to {
-              target.name} with {self.weapon.name}")
+        target.health_bar.update()
+        print(f"{self.name} dealt {self.weapon.damage} to "
+              f"{target.name} with {self.weapon.name}")
 
 
 class Hero(Character):
@@ -27,10 +29,27 @@ class Hero(Character):
                  ) -> None:
         super().__init__(name=name, health=health)
 
+        self.default_weapon = self.weapon
+        self.health_bar = HealthBar(self, color='green2')
+    
+    def equip(self, weapon) -> None:
+        self.weapon = weapon
+        print(f'{self.name} equipped a(n) {self.weapon.name}!')
+
+    def drop(self) -> None:
+        if self.weapon.name != self.default_weapon.name:
+            print(f'{self.name} dropped the {self.weapon.name}!')
+            self.weapon = self.default_weapon
+        
+
 
 class Enemy(Character):
     def __init__(self,
                  name: str,
                  health: int,
+                 weapon,
                  ) -> None:
         super().__init__(name=name, health=health)
+
+        self.weapon = weapon
+        self.health_bar = HealthBar(self, color='red')
